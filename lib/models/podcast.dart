47fee4c19cbf55dd1caf3e3c2297acea15d68c55
amn86 <aminianafrooz86@gmail.com>
2021-06-09 18:48:53 +0200

    first push

--- lib/models/podcast.dart
+++ lib/models/podcast.dart
@@ -8,4 +8,27 @@ class Podcast {
 
   Podcast(this.artistName, this.id, this.imageUrl, this.musicName,
       {this.isLiked = false, this.length = 0});
+
+  Podcast fromJson(Map<String, dynamic> json){
+    return Podcast(
+      json['id'],
+      json['imageUrl'],
+      json['artistName'],
+      json['musicName'],
+      json['isLiked'],
+      json['length'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['imageUrl'] = this.imageUrl;
+    data['artistName'] = this.artistName;
+    data['musicName'] = this.musicName;
+    data['isLiked'] = this.isLiked;
+    data['length'] = this.length;
+    return data;
+  }
+
 }
--- lib/models/youtube_item.dart
+++ lib/models/youtube_item.dart
@@ -5,4 +5,23 @@ class YoutubeItem {
   String title;
 
   YoutubeItem(this.url, this.id, this.imageUrl, this.title);
+
+  YoutubeItem fromJson(Map<String, dynamic> json) {
+    return YoutubeItem(
+      json['id'],
+      json['imageUrl'],
+      json['url'],
+      json['title'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['imageUrl'] = this.imageUrl;
+    data['url'] = this.url;
+    data['title'] = this.title;
+    return data;
+  }
+
 }
