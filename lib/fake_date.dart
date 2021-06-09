bca5b40a6647321c53c7f125c0ba0b4e1c449748
Alireza <alirezaaslami@gmail.com>
2021-06-09 21:19:05 +0430

    notifiers

--- lib/fake_date.dart
+++ lib/fake_date.dart
@@ -46,47 +46,68 @@ List<Podcast> podcastList() {
 
 List<Gallery> galleyList() {
   List<Gallery> galleries = [];
-  galleries.add(Gallery(0, "assets/Radio8.jpg", "Radio Tiam _ Audiogate ",
-      "subtitle"));
-  galleries.add(Gallery(1, "assets/PhotoHoghogheBashar.jpg",
-      "The first meeting at Radio Orange Studio", "subtitle", ));
+  galleries.add(
+      Gallery(0, "assets/Radio8.jpg", "Radio Tiam _ Audiogate ", "subtitle"));
   galleries.add(Gallery(
-      2,
-      "assets/firstPhoto.jpg",
-      "The first performance of Sahar on the radio",
-      "subtitle",));
-  galleries.add(Gallery(
-      3,
-      "assets/Radio3.jpg",
-      "The first meeting on the relationship between culture and history",
-      "subtitle",
-));
-  galleries.add(Gallery(
-      4,
-      "assets/photoGallery.jpg",
-      "The second meeting with Armin about traditional music",
-      "subtitle",
+    1,
+    "assets/PhotoHoghogheBashar.jpg",
+    "The first meeting at Radio Orange Studio",
+    "subtitle",
   ));
   galleries.add(Gallery(
-      5,
-      "assets/Radio1.jpg",
-      "The second meeting on the relationship between culture and history",
-      "subtitle",
+    2,
+    "assets/firstPhoto.jpg",
+    "The first performance of Sahar on the radio",
+    "subtitle",
   ));
-  galleries.add(Gallery(6, "assets/Radio2.jpg",
-      "The first meeting with mr. Wikler", "subtitle", ));
-  galleries.add(Gallery(7, "assets/Radio3.jpg",
-      "The first interview with Babak", "subtitle", ));
-  galleries.add(Gallery(8, "assets/Radio4.jpg",
-      "The first meeting with mr. Kasra", "subtitle", ));
   galleries.add(Gallery(
-      9,
-      "assets/Radio6.JPG",
-      "The first New Year's performance with Sahar",
-      "subtitle",
-      ));
-  galleries.add(Gallery(10, "assets/Radio7.JPG",
-      "Live radio program in Radio Orange Studio", "subtitle", ));
+    3,
+    "assets/Radio3.jpg",
+    "The first meeting on the relationship between culture and history",
+    "subtitle",
+  ));
+  galleries.add(Gallery(
+    4,
+    "assets/photoGallery.jpg",
+    "The second meeting with Armin about traditional music",
+    "subtitle",
+  ));
+  galleries.add(Gallery(
+    5,
+    "assets/Radio1.jpg",
+    "The second meeting on the relationship between culture and history",
+    "subtitle",
+  ));
+  galleries.add(Gallery(
+    6,
+    "assets/Radio2.jpg",
+    "The first meeting with mr. Wikler",
+    "subtitle",
+  ));
+  galleries.add(Gallery(
+    7,
+    "assets/Radio3.jpg",
+    "The first interview with Babak",
+    "subtitle",
+  ));
+  galleries.add(Gallery(
+    8,
+    "assets/Radio4.jpg",
+    "The first meeting with mr. Kasra",
+    "subtitle",
+  ));
+  galleries.add(Gallery(
+    9,
+    "assets/Radio6.JPG",
+    "The first New Year's performance with Sahar",
+    "subtitle",
+  ));
+  galleries.add(Gallery(
+    10,
+    "assets/Radio7.JPG",
+    "Live radio program in Radio Orange Studio",
+    "subtitle",
+  ));
 
   return galleries;
 }
